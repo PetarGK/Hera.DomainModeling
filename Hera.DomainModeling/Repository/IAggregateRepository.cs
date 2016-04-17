215ffde89fecdb2afa11198bce3284be3da2d9e7
Petar Korudzhiev <petar.korudzhiev@gmail.com>
2016-04-17 22:25:29 +0300

    Added repository concept

---
+++ Hera.DomainModeling/Repository/IAggregateRepository.cs
@@ -0,0 +1,11 @@
+﻿using Hera.DomainModeling.AggregareRoot;
+using Hera.DomainModeling.Identity;
+
+namespace Hera.DomainModeling.Repository
+{
+    public interface IAggregateRepository
+    {
+        void Save<TAggregateRoot>(TAggregateRoot aggregateRoot, string bucketId) where TAggregateRoot : IAggregateRoot;
+        TAggregateRoot Load<TAggregateRoot>(IIdentity aggregateRootId, string bucketId) where TAggregateRoot : IAggregateRoot;
+    }
+}
